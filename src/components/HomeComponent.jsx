13d057e70c32e83db56998e3945e37fb20cf1cdf
Srijit Bhattacharya <56132120+srijit43@users.noreply.github.com>
2023-12-19 22:51:31 -0800

    Update HomeComponent.jsx
    Removed button import

--- src/components/HomeComponent.jsx
+++ src/components/HomeComponent.jsx
@@ -1,5 +1,4 @@
 import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
